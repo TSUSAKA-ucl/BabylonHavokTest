b7cd1454dcab16f52e39da588acb004d28ef2c31
Yuji Tsusaka <tsusaka@ucl.nuee.nagoya-u.ac.jp>
2025-09-08 12:09:28 +0900

    右コントローラーをsqueezeするとboxがコントローラーにくっつく

--- src/main.js
+++ src/main.js
@@ -30,6 +30,9 @@ async function initScene() {
     { mass: 0, restitution: 0.5 },
     scene
   );
+  const groundMat = new BABYLON.StandardMaterial('gmat', scene);
+  groundMat.diffuseColor = new BABYLON.Color3(0.2, 0.6, 0.2);
+  ground.material = groundMat;
 
   // 箱
   const box = BABYLON.MeshBuilder.CreateBox("box", { size: 1 }, scene);
@@ -40,6 +43,9 @@ async function initScene() {
     { mass: 1, restitution: 0.7 },
     scene
   );
+  const boxMat = new BABYLON.StandardMaterial('bmat', scene);
+  boxMat.diffuseColor = new BABYLON.Color3(0.95, 0.0, 0.0);
+  box.material = boxMat;
 
   // XR (VR) エクスペリエンス
   const xr = await scene.createDefaultXRExperienceAsync({
@@ -48,11 +54,49 @@ async function initScene() {
 
   // VR コントローラ追従サンプル（右手に箱を追従させる）
   xr.input.onControllerAddedObservable.add((controller) => {
-    if (controller.inputSource.handedness === "right") {
-      controller.grip.position = box.position.clone();
-      controller.grip.rotationQuaternion = box.rotationQuaternion?.clone() ?? BABYLON.Quaternion.Identity();
-      // 将来的に掴み/放す処理を追加可能
-    }
+    console.log('Controller added:', controller.uniqueId);
+    controller.onMotionControllerInitObservable.add((motionController) => {
+      // motionController.onMeshLoadedObservable.add((model) => {});
+      // const xr_ids = motionController.getComponentIds();
+      // for (let i=0;i<xr_ids.length;i++){
+      // 	console.log(controller.uniqueId + "::"+xr_ids[i]);
+      // }
+      if (controller.uniqueId === 'controller-0-tracked-pointer-right') {
+	const squeezeComponent = motionController.getComponent("xr-standard-squeeze");
+	console.log('squeezeComponent:'+squeezeComponent);
+	if (squeezeComponent.isButton()) {
+	  console.log('squeezeComponent has a button value');
+	}
+	if (squeezeComponent.isAxes()) {
+	  console.log('squeezeComponent has axes data');
+	}
+	
+	const controllerMesh = BABYLON.MeshBuilder.CreateSphere('ctrl-'+controller.uniqueId,
+								{diameter: 1.2}, scene);
+	// attach to grip if present
+	controllerMesh.parent = controller.grip || controller.pointer;
+	controllerMesh.position = new BABYLON.Vector3(0,0,0);
+
+	if (controller.inputSource && controller.inputSource.hand) {
+	  console.log("Hand tracking controller detected!");
+	}
+	// 将来的に掴み/放す処理を追加可能
+	// box をコントローラに追従させる
+	scene.onBeforeRenderObservable.add(() => {
+	  // console.log('squeeze button value:'+squeezeComponent.value);
+	  if (squeezeComponent.value > 0.75) {
+	    if (controller.grip) {
+	      box.position.copyFrom(controller.grip.position);
+	      box.rotationQuaternion = controller.grip.rotationQuaternion;
+	    }
+ // else if (controller.pointer) {
+ // 	      box.position.copyFrom(controller.pointer.position);
+ // 	      box.rotationQuaternion = controller.pointer.rotationQuaternion;
+ // 	    }
+	  }
+	});
+      }
+    });
   });
 
   engine.runRenderLoop(() => scene.render());
